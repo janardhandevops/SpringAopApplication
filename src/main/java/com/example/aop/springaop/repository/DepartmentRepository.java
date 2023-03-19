fabe94b5d44857e253059b59a96acd26848a9802
janardhandevops <springkart2022@gmail.com>
2023-03-19 11:15:03 +0530

    added pom.xml changes

--- src/main/java/com/example/aop/springaop/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.aop.springaop.repository;
-
-import com.example.aop.springaop.model.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<Department,Integer> {
-}
